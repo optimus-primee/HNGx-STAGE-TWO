24b6bfb1417d4a82eb9d7b277477308af9cd7672
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2023-09-15 19:51:42 +0100

    commit

--- app/components/FeaturedMovies.tsx
+++ app/components/FeaturedMovies.tsx
@@ -1,146 +1,113 @@
-"use client";
+"use client"
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Loading from "./Loading";
+import Loading from "../components/Loading";
 import Link from "next/link";
-import imdb from "../assests/images/imdb.png";
-import Image from "next/image";
-import tom from "../assests/images/tom.png";
-import Bottom from "./bottom";
+import { useSearchParams } from "next/navigation";
+import axios from "axios";
 
-function FeaturedMovies() {
-  const [movies, setMovies] = useState<IMovie[] | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+import { BsPlayFill } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
+import dynamic from "next/dynamic";
+import Search from "../components/Search";
 
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
+const FeaturedMovies = () => {
   interface IMovie {
     poster_path: string;
-    id: string;
-    imdb_id: string | null;
-    title: string;
-    vote_average: string;
-    release_date: string;
-    genre_ids: number[]; // Change genre_ids to be an array of numbers
     backdrop_path: string;
-    randomPercentage: number;
+    title: string;
+    genres: [
+      {
+        name: string;
+        id: string;
+      }
+    ];
+    original_language: string;
+    release_date: string;
+    runtime: string;
+    vote_average: string;
+    overview: string;
+    videos: { results: [{ type: string; key: string }] };
+    id: string;
   }
-  const genreMap: { [key: number]: string } = {
-    28: "Action",
-    12: "Adventure",
-    16: "Animation",
-    35: "Comedy",
-    80: "Crime",
-    99: "Documentary",
-    18: "Drama",
-    10751: "Family",
-    14: "Fantasy",
-    36: "History",
-    27: "Horror",
-    10402: "Music",
-    9648: "Mystery",
-    10749: "Romance",
-    878: "Science Fiction",
-    10770: "TV Movie",
-    53: "Thriller",
-    10752: "War",
-    37: "Western",
-  };
 
-  // Function to get the genre names for a movie
-  const getGenreNames = (genreIds: number[]): string[] => {
-    return genreIds.map((genreId) => genreMap[genreId]);
-  };
-  // Function to generate a random percentage between min and max
-  const getRandomPercentage = (min: number, max: number): number => {
-    return Math.random() * (max - min) + min;
-  };
+
+  const [movies, setMovies] = useState<IMovie[]>([]); // Use an array to store multiple movies
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPlayer, setShowPlayer] = useState(false);
+  const [trailer, setTrailer] = useState("");
+  const [movie, setMovie] = useState<IMovie>();
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/movie/top_rated?api_key=ca0825715c06ebe0d0621ba9ead36000&language=en-US&page=1`
-        );
+    setIsLoading(true);
 
-        const moviesWithImdbId = await Promise.all(
-          response.data.results.map(async (movie: IMovie) => {
-            const imdbResponse = await axios.get(
-              `https://api.themoviedb.org/3/movie/${movie.id}?api_key=ca0825715c06ebe0d0621ba9ead36000`
-            );
-            movie.id = imdbResponse.data.id || null;
-            movie.randomPercentage = getRandomPercentage(0, 100); // Generate a random percentage
-            return movie;
-          })
-        );
+    
+    axios
+      .get(`https://api.themoviedb.org/3/movie/top_rated?api_key=ca0825715c06ebe0d0621ba9ead36000&language=en-US&page=1`)
+      .then((res) => {
+        // Extract the array of movie results
+        const movieResults = res?.data?.results || [];
 
-        setMovies(moviesWithImdbId);
-       console.log(moviesWithImdbId);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching movies:", error);
-        setIsLoading(false);
-      }
-    };
+        // Store the movie results in the state
+        setMovies(movieResults);
 
-    fetchMovies();
+        // Assuming you want to load details for the first movie initially
+        if (movieResults.length > 0) {
+          loadMovieDetails(movieResults[0].id);
+        } else {
+          setIsLoading(false);
+        }
+      });
   }, []);
 
+  const loadMovieDetails = (movieId: number) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=ca0825715c06ebe0d0621ba9ead36000&append_to_response=videos`
+      )
+      .then((res) => {
+        setMovie(res.data);
+        setIsLoading(false);
+        console.log(res.data);
+      });
+  };
+
   return (
-    <div className="sm:px-24 px-6">
-      <div>
-        <h2 className="sm:text-[36px] text-[28px] font-bold my-8">
-          Featured Movie
-        </h2>
+    <div className="">
+      <div className="sm:hidden flex pt-3 mx-4 w-[100%]">
+        
       </div>
-      <div className="grid sm:grid-cols-4 w-[100%] sm:gap-y-20 gap-y-20  overflow-x-hidden">
-        {isLoading ? (
-          <Loading />
-        ) : (
-          movies &&
-          movies.map((movie: IMovie) => (
-            <div key={movie.id}>
-              {movie.id && (
-                <Link
-                  href={`/movie/${movie.id}`}
-                  title={`More information about ${movie.title}`}
-                >
-                  <div className="sm:w-[250px] h-[370px] w-[100%] ">
-                    <img
-                      src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
-                      alt={movie.title}
-                      className="w-[100%] h-[100%] object-cover "
-                      onLoad={() => setIsLoading(false)}
-                    />
-                  </div>
-                  <h2 className="text-[#9CA3AF] text-[12px] font-bold mt-3">
-                    {movie.release_date}
-                  </h2>
-                  <h2 className="text-[#4f70b7] text-sm my-3">{movie.title}</h2>
-                  <div className="flex justify-between items-center sm:w-[250px] w-[100%]">
-                    <div className="flex gap-2 items-center">
-                      <Image src={imdb} alt="" width={35} height={17} />
-                      <h2 className="text-[#9CA3AF] text-[12px]">
-                        {movie.vote_average}/100
-                      </h2>
-                    </div>
-                    <div className="flex gap-2">
-                      <Image src={tom} alt="" width={16} height={17} />
-                      <h2 className="text-[#9CA3AF] text-[12px]">
-                        {movie.randomPercentage.toFixed(1)}%
-                      </h2>
-                    </div>
-                  </div>
-                  <h2 className="text-[#9CA3AF] text-[12px] font-bold mt-3">
-                    {getGenreNames(movie.genre_ids).join(", ")}
-                  </h2>
-                </Link>
-              )}
+      {isLoading && <Loading />}
+
+      <div className="">
+        <div className="relative sm:px-24 px-4 pt-32 grid gap-4 sm:grid-cols-4 w-[100%]">
+          {movies.map((movie) => (
+            <div key={movie.id} className="mx-auto flex-none relative">
+              {/* Render movie details for each movie in the 'movies' array */}
+              <Link
+                href={`/movie/${movie.id}`}
+                title={`More information about ${movie.title}`}
+              >
+                <div className="sm:w-[250px] h-[370px] w-[100%] ">
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+                    alt={movie.title}
+                    className="w-[100%] h-[100%] object-cover "
+                    onLoad={() => setIsLoading(false)} // Update isImgLoading here
+                  />
+                </div>
+              </Link>
             </div>
-          ))
-        )}
+          ))}
+
+          {/* ... Other movie details */}
+        </div>
+
+        {/* ... React Player */}
       </div>
     </div>
   );
-}
+};
 
 export default FeaturedMovies;
